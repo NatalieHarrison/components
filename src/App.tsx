bff4d1ffc4e1b7c178ccf0453f58ff00e5a38cad
NatalieHarrison <xnataaliee@aol.com>
2022-07-05 13:57:28 -0400

    Green Boxes: Creating an array to make dynamic

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,13 @@ import { Box } from '@mui/system';
 
 import CheckBox from './components/CheckBox';
 import FullGreenBoxes from './components/FullGreenBox';
-import GreenBox from './components/GreenBox';
+import GreenBoxes from './components/GreenBox';
 import GreyBoxes from './components/GreyBox';
 import GroupFullGreenBox from './components/GroupFullGreenBox';
 import Header from './components/Header';
 import NameHeader from './components/NameHeader';
 import Search from './components/Search';
 import SideBar from './components/sidebar';
-import SingleBox from './components/SingleBox';
 import UserInfo from './components/UserInfo';
 import UserInfoSet from './componentSets/UserInfoSet';
 
@@ -21,33 +20,23 @@ const content: any = {
   // header: 'Products & Services that Delight',
   greyBoxItems: [{ title: 'Total Open Problems', body: '329,778', num: '+5,690' }],
   fullGreenBoxItems: [{ title: 'Direct Deposit', body: 'Active', num: '' }],
-
-  // title2: 'Member Churn This Period',
-  // body2: '1,291',
-  // num2: '+421',
-  // title3: 'Acquisition to Churn Ratio',
-  // body3: '3.78',
-  // num3: '+0.37',
-
-  // title: 'Total ',
-  // body: '230,827',
-  // num: '+4',
-
+  greenBoxItems: [{ title: 'Member Tenure', body: '14 Years', num: '' },
+  { title: 'Member Tenure', body: '14 Years', num: '' }],
   // //name header
-  // firstName: 'Jane',
-  // lastName: 'Anderson',
+  firstName: 'Jane',
+  lastName: 'Anderson',
   // //user info
-  // openDate: '05/31/2007',
-  // activeStatus: 'True',
-  // qualificationReason: 'SEG (IBM)',
-  // location: 'Chapel Hill, NC',
-  // generation: 'Y',
-  // gender: 'Male',
-  // memberType: 'Hybrid (Branch + Digital',
-  // preferredBranch: "St. Alban's",
-  // preferredATM: "St. Alban's",
-  // totLTProducts: '7',
-  // openProducts: '4',
+  openDate: '05/31/2007',
+  activeStatus: 'True',
+  qualificationReason: 'SEG (IBM)',
+  location: 'Chapel Hill, NC',
+  generation: 'Y',
+  gender: 'Male',
+  memberType: 'Hybrid (Branch + Digital',
+  preferredBranch: "St. Alban's",
+  preferredATM: "St. Alban's",
+  totLTProducts: '7',
+  openProducts: '4',
 };
 function App() {
   return (
@@ -60,13 +49,9 @@ function App() {
 
         <Box sx={{ mt: 7, width: 700 }}>
           <Stack direction="row">
-            <GreenBox {...content}></GreenBox>
+            <GreenBoxes {...content}></GreenBoxes>
             <FullGreenBoxes {...content}></FullGreenBoxes>
           </Stack>
-        </Box>
-
-        <Box sx={{ mt: 7, width: 700 }}>
-          <GroupFullGreenBox {...content}></GroupFullGreenBox>
         </Box>
 
         <Box sx={{ mt: 7, width: 700 }}>
--- src/components/GreenBox.tsx
+++ src/components/GreenBox.tsx
@@ -14,7 +14,29 @@ interface GreenBoxProps {
   num: string;
 }
 
+interface GreenBoxesProps {
+  greenBoxItems: GreenBoxProps[];
+}
 const GreenBox = (props: GreenBoxProps) => {
+  return (
+    <Paper elevation={4}>
+      <Card sx={{ width: 166.5, height: 90 }}>
+        <CardContent sx={{ backgroundColor: '#4caf50', height: 10 }}>
+          <Typography color="#f5f5f5">{props.title}</Typography>
+        </CardContent>
+
+        <CardContent>
+          <Stack>
+            <Stack direction="row" justifyContent="center">
+              <Typography color="primary"> {props.body}</Typography>
+            </Stack>
+          </Stack>
+        </CardContent>
+      </Card>
+    </Paper>
+  );
+};
+const GreenBoxes = (props: GreenBoxesProps) => {
   const theme = createTheme({
     palette: {
       primary: {
@@ -26,24 +48,10 @@ const GreenBox = (props: GreenBoxProps) => {
     <Container>
       <ThemeProvider theme={theme}>
         <Grid container spacing={0} direction="row" sx={{ mt: 2 }}>
-          <Paper elevation={4}>
-            <Card sx={{ width: 166.5, height: 90 }}>
-              <CardContent sx={{ backgroundColor: '#4caf50', height: 10 }}>
-                <Typography color="#f5f5f5">{props.title}</Typography>
-              </CardContent>
-
-              <CardContent>
-                <Stack>
-                  <Stack direction="row" justifyContent="center">
-                    <Typography color="primary"> {props.body}</Typography>
-                  </Stack>
-                </Stack>
-              </CardContent>
-            </Card>
-          </Paper>
+          {props.greenBoxItems && props.greenBoxItems.map((box) => <GreenBox {...box}> </GreenBox>)}
         </Grid>
       </ThemeProvider>
     </Container>
   );
 };
-export default GreenBox;
+export default GreenBoxes;
