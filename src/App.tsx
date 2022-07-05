50e9d11c19c3521993f2769e8f4cb9f432220412
NatalieHarrison <xnataaliee@aol.com>
2022-07-05 13:24:36 -0400

    Import fix

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import './App.css';
 import { Card, CardHeader, Grid, CardContent, Stack, Typography, Paper } from '@mui/material';
 import { Box, Container } from '@mui/system';
 
-import Boxes from './components/Boxes';
+import GreyBoxes from './components/GreyBox';
 import CheckBox from './components/CheckBox';
 import FullGreenBox from './components/FullGreenBox';
 import GreenBox from './components/GreenBox';
@@ -17,39 +17,44 @@ import SingleBox from './components/SingleBox';
 import UserInfo from './components/UserInfo';
 import UserInfoSet from './componentSets/UserInfoSet';
 
+
 const content: any = {
-  header: 'Products & Services that Delight',
-  items: [
-    { title: 'Total Open Problems', body: '329,778', num: '+5,690' },
-    { title: 'Total Open Problems', body: '329,778', num: '+5,690' },
+  // header: 'Products & Services that Delight',
+  greyBoxItems: [
+  {title: 'Total Open Problems',
+  body: '329,778',
+  num: '+5,690'},
+  {title: 'Total Open Problems',
+  body: '329,778',
+  num: '+5,690'},
 
-    // title2: 'Member Churn This Period',
-    // body2: '1,291',
-    // num2: '+421',
-    // title3: 'Acquisition to Churn Ratio',
-    // body3: '3.78',
-    // num3: '+0.37',
+  // title2: 'Member Churn This Period',
+  // body2: '1,291',
+  // num2: '+421',
+  // title3: 'Acquisition to Churn Ratio',
+  // body3: '3.78',
+  // num3: '+0.37',
 
-    // title: 'Total ',
-    // body: '230,827',
-    // num: '+4',
+  // title: 'Total ',
+  // body: '230,827',
+  // num: '+4',
 
-    // //name header
-    // firstName: 'Jane',
-    // lastName: 'Temple',
-    // //user info
-    // openDate: '05/31/2007',
-    // activeStatus: 'True',
-    // qualificationReason: 'SEG (IBM)',
-    // location: 'Chapel Hill, NC',
-    // generation: 'Y',
-    // gender: 'Male',
-    // memberType: 'Hybrid (Branch + Digital',
-    // preferredBranch: "St. Alban's",
-    // preferredATM: "St. Alban's",
-    // totLTProducts: '7',
-    // openProducts: '4',
-  ],
+  // //name header
+  // firstName: 'Jane',
+  // lastName: 'Temple',
+  // //user info
+  // openDate: '05/31/2007',
+  // activeStatus: 'True',
+  // qualificationReason: 'SEG (IBM)',
+  // location: 'Chapel Hill, NC',
+  // generation: 'Y',
+  // gender: 'Male',
+  // memberType: 'Hybrid (Branch + Digital',
+  // preferredBranch: "St. Alban's",
+  // preferredATM: "St. Alban's",
+  // totLTProducts: '7',
+  // openProducts: '4',
+  ]
 };
 function App() {
   return (
